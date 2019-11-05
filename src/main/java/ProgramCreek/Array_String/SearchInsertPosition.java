924bd54a291d947b94ae3a5087fbe92783262cab
Zia Khalid <ziakhalid@Zias-MacBook-Pro.local>
2019-11-05 23:52:51 +0800

    SearchInsertPosition

---
+++ src/main/java/ProgramCreek/Array_String/SearchInsertPosition.java
@@ -0,0 +1,34 @@
+package ProgramCreek.Array_String;
+
+//        [1,3,5,6], 5 -> 2
+//        [1,3,5,6], 2 -> 1
+//        [1,3,5,6], 7 -> 4
+//        [1,3,5,6], 0 -> 0
+
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+
+        if(nums ==null || nums.length<1){
+            return 0;
+        }
+
+        int first = 0;
+        int last = nums.length - 1;
+        int mid;
+
+        while (first<=last){
+            mid = (first+last)/2;
+
+            if(nums[mid]>target){
+                last = mid-1;
+            }else if(nums[mid]<target){
+                first = mid+1;
+            }else {
+                return mid;
+            }
+        }
+
+        return first;
+    }
+}
---
+++ src/test/java/ProgramCreek/Array_String/SearchInsertPositionTest.java
@@ -0,0 +1,43 @@
+package ProgramCreek.Array_String;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearchInsertPositionTest {
+
+    SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+
+    @Test
+    public void searchInsertPosition1(){
+        int[] inputArr = {1,3,5,6};
+        int inputNum = 5;
+        int expected = 2;
+        assertEquals(expected, searchInsertPosition.searchInsert(inputArr, inputNum));
+    }
+
+    @Test
+    public void searchInsertPosition2(){
+        int[] inputArr = {1,3,5,6};
+        int inputNum = 2;
+        int expected = 1;
+        assertEquals(expected, searchInsertPosition.searchInsert(inputArr, inputNum));
+    }
+
+    @Test
+    public void searchInsertPosition3(){
+        int[] inputArr = {1,3,5,6};
+        int inputNum = 7;
+        int expected = 4;
+        assertEquals(expected, searchInsertPosition.searchInsert(inputArr, inputNum));
+    }
+
+    @Test
+    public void searchInsertPosition4(){
+        int[] inputArr = {1,3,5,6};
+        int inputNum = 0;
+        int expected = 0;
+        assertEquals(expected, searchInsertPosition.searchInsert(inputArr, inputNum));
+    }
+
+}
